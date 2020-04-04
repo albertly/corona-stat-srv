50226b4f67ff7935dc0c417da624623d1824867a
albertly <albertly@yandex.ru>
2020-04-04 20:30:18 +0300

    Cache

--- services/stat.service.js
+++ services/stat.service.js
@@ -1,43 +1,59 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+const NodeCache = require("node-cache");
+
+const myCache = new NodeCache({ stdTTL: 30, checkperiod: 120 });
+const keyToday = 'main_table_countries_today';
+const keyYesterday = 'main_table_countries_yesterday';
 
 exports.getStat = function (today = true) {
 
   const url = 'https://www.worldometers.info/coronavirus';
 
-  console.log('axios start 1');
-  return axios.get(url).then(r => {
-    console.log('axios end 1');
-    const reply = [];
+  const key = today ? keyToday : keyYesterday;
 
-    const $ = cheerio.load(r.data, {
-      xml: {
-        normalizeWhitespace: true,
-      }
-    });
+  const cacheValue = myCache.get(key);
 
-    const fields = ['country', 'total', 'new', 'totalDeaths', 'newDeaths', 'totalRecovered', 'active', 'serious', 'totCasesPer1m', , , , , ,];
+  if (cacheValue == undefined) {
+    console.log('axios start 1');
+    return axios.get(url).then(r => {
+      console.log('axios end 1');
+      const reply = [];
 
-    let tableName = '#main_table_countries_today';
-    if (!today) {
-      tableName = '#main_table_countries_yesterday'
-    }
-    const data = $(`${tableName} > tbody > tr`);
-
-    data.each(function (i) {
-      if (i !== 0) {
-        let obj = {};
-        $(this).find('td').each(function (j) {
-          if (fields[j]) {
-            obj[fields[j]] = $(this).text();
-          }
-        })
-        if (obj.county !== 'World') {
-          reply.push(obj);
+      const $ = cheerio.load(r.data, {
+        xml: {
+          normalizeWhitespace: true,
         }
-      }
-    });
+      });
 
-    return reply;
-  });
+      const fields = ['country', 'total', 'new', 'totalDeaths', 'newDeaths', 'totalRecovered', 'active', 'serious', 'totCasesPer1m', , , , , ,];
+
+      let tableName = '#main_table_countries_today';
+      if (!today) {
+        tableName = '#main_table_countries_yesterday'
+      }
+      const data = $(`${tableName} > tbody > tr`);
+
+      data.each(function (i) {
+        if (i !== 0) {
+          let obj = {};
+          $(this).find('td').each(function (j) {
+            if (fields[j]) {
+              obj[fields[j]] = $(this).text();
+            }
+          })
+          if (obj.county !== 'World') {
+            reply.push(obj);
+          }
+        }
+      });
+
+      myCache.set(key, reply);
+      return reply;
+    });
+  }
+
+  console.log('cache hitted');
+  return cacheValue;
+
 }
